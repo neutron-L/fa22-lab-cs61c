9cf769de5971b4c0f9462819c7db482e0ead8460
rda <2055017970@qq.com>
2023-01-10 21:10:29 +0800

    all pass

--- lab03/discrete_fn.s
+++ lab03/discrete_fn.s
@@ -1,11 +1,15 @@
 .globl f # this allows other files to find the function f
+.word   6, 61, 17, -38, 19, 42, 5
 
 # f takes in two arguments:
 # a0 is the value we want to evaluate f at
 # a1 is the address of the "output" array (defined above).
 # The return value should be stored in a0
 f:
-
+    addi a0, a0, 3
+    slli a0, a0, 2
+    add a1, a1, a0
+    lw a0, 0(a1)
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
     jr ra  
--- lab03/factorial.s
+++ lab03/factorial.s
@@ -1,7 +1,7 @@
 .globl factorial
 
 .data
-n: .word 8
+n: .word 7
 
 .text
 # Don't worry about understanding the code in main
@@ -27,7 +27,14 @@ main:
 # The return value should be stored in a0
 factorial:
     # YOUR CODE HERE
-
+    add a1, a0, x0 # a1 = a0
+    addi a0, x0, 1  # a0 = 1
+loop:
+    beq a1, x0, exit
+    mul a0, a0, a1
+    addi a1, a1, -1 # decrement counter
+    j loop
+exit:
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
     jr ra
